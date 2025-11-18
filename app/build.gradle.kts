df0ce56c5f17afdee74859eb229c914341e41cc9
fraim <fraimework@gmail.com>
2025-11-18 17:28:26 +0700

    update design

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -15,8 +15,8 @@ android {
         applicationId = "com.sunguard.vault"
         minSdk = 24  // Android 7.0
         targetSdk = 36
-        versionCode = 1
-        versionName = "1.0.0"
+        versionCode = 2
+        versionName = "1.0.1"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
